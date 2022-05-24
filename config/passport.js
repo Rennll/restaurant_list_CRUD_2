d24becffade355b86e32169e2233683f7e5c96f1
Rennll <dt616717@gmail.com>
2022-05-24 18:47:42 +0800

    feat: add user authenticate in restaurant page

--- config/passport.js
+++ config/passport.js
@@ -9,19 +9,20 @@ module.exports = app => {
   app.use(passport.session())
 
   // set strategies
-  passport.use(new LocalStrategy({ usernameField: 'email' }, (email, password, done) => {
-    User.findOne({ email })
-      .then(user => {
-        if (!user) {
-          return done(null, false, { message: 'This email is not registered.' })
-        }
-        if (user.password !== password) {
-          return done(null, false, { message: 'Email or password is incorrect.' })
-        }
-        return done(null, user)
-      })
-      .catch(err => done(err, false))
-  }))
+  passport.use(new LocalStrategy({ usernameField: 'email' },
+    (email, password, done) => {
+      User.findOne({ email })
+        .then(user => {
+          if (!user) {
+            return done(null, false, { message: 'This email is not registered.' })
+          }
+          if (user.password !== password) {
+            return done(null, false, { message: 'Email or password is incorrect.' })
+          }
+          return done(null, user)
+        })
+        .catch(err => done(err, false))
+    }))
 
   // set serialize and deserialize
   passport.serializeUser((user, done) => {
