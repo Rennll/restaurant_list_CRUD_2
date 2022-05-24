b966d5a9e832950a9bc4b50014c6ea36e045a57c
Rennll <dt616717@gmail.com>
2022-05-24 16:48:01 +0800

    feat: add register func

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const router = express.Router()
-
+const User = require('../../models/user')
 router.get('/login', (req, res) => {
   res.render('login')
 })
@@ -14,7 +14,33 @@ router.get('/register', (req, res) => {
 })
 
 router.post('/register', (req, res) => {
-  // 存取user
+  const { name, email, password, confirmedPassword } = req.body
+  User.findOne({ email })
+    .then(user => {
+      if (user) {
+        return res.render('register', {
+          name,
+          email,
+          password,
+          confirmedPassword
+        })
+      }
+      if (password !== confirmedPassword) {
+        return res.render('register', {
+          name,
+          email,
+          password,
+          confirmedPassword
+        })
+      }
+      return User.create({
+        name,
+        email,
+        password
+      })
+        .then(() => res.redirect('/users/login'))
+    })
+    .catch(err => console.error(err))
 })
 
 router.get('/logout', (req, res) => {
