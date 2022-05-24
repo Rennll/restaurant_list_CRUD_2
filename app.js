e7676d67bf426193be8539b41a5854b10c89c17b
Rennll <dt616717@gmail.com>
2022-05-24 19:02:13 +0800

    feat: add different display with user isAuthenticated

--- app.js
+++ app.js
@@ -24,6 +24,11 @@ app.use(session({
   saveUninitialized: true
 }))
 usePassport(app)
+app.use((req, res, next) => {
+  res.locals.isAuthenticated = req.isAuthenticated()
+  res.locals.user = req.user
+  next()
+})
 
 // setting routes
 app.use(routes)
